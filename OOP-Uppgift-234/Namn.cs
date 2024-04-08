2bf9d46925e08ca98b1281d2bccecafac74b4514
HerrBergenholtz <zbdv8800@gmail.com>
2024-04-08 11:32:09 +0200

    Add project files.

---
+++ OOP-Uppgift-234/Namn.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Uppgift_2
+{
+    internal class Namn
+    {
+        private string förnamn;
+        private string efternamn;
+        private DateTime datum;
+        private float vikt;
+        private float höjd;
+
+        public Namn(string fn, string en, DateTime dat, float vk, float hj)
+        {
+            förnamn = fn;
+            efternamn = en;
+            datum = dat;
+            vikt = vk;
+            höjd = hj;
+        }
+
+        public string SetNamn
+        {
+            get { return förnamn + " " + efternamn; }
+            set { förnamn = value.Split(" ")[0]; efternamn = value.Split(" ")[1]; }
+        }
+
+        public DateTime setDatum
+        {
+            get { return datum; }
+            set { datum = value; }
+        }
+
+        public bool KontrolleraDatum()
+        {
+            if (datum < DateTime.Now.AddYears(-18))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public float SetHöjd
+        {
+            get { return höjd; }
+            set { höjd = value; }
+        }
+
+        public float SetVikt
+        {
+            get { return vikt; }
+            set { vikt = value; }
+        }
+
+        public float BMI()
+        {
+            höjd /= 100;
+            return vikt / (höjd * höjd);
+        }
+    }
+}
---
+++ OOP-Uppgift-234/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Uppgift_2;
+using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Namn namn2 = new("David", "Davidsson", new DateTime(1990, 01, 01), 60, 175);
+            Console.WriteLine(namn2.SetNamn + " " + namn2.setDatum + " " + namn2.KontrolleraDatum() + " " + namn2.BMI());
+        }
+    }
+}
